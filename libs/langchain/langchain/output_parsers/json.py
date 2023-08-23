0afcb4d1ea176a17c51850530b7437f5201a5abf
ts003428300 <34190979+ts003428300@users.noreply.github.com>
2023-08-23 20:04:13 +0800

    Update json.py
    There is a bug in the function parse_json_markdown.
    When the input json_string contains ``` $code ``` . It mistakenly interprets $code as json format and fails to parse.

--- libs/langchain/langchain/output_parsers/json.py
+++ libs/langchain/langchain/output_parsers/json.py
@@ -49,14 +49,14 @@ def parse_json_markdown(json_string: str) -> dict:
         The parsed JSON object as a Python dictionary.
     """
     # Try to find JSON string within triple backticks
-    match = re.search(r"```(json)?(.*)```", json_string, re.DOTALL)
+    match = re.findall(r"```(json)?(.*?)```", json_string, re.DOTALL)
 
     # If no match found, assume the entire string is a JSON string
     if match is None:
         json_str = json_string
     else:
         # If match found, use the content within the backticks
-        json_str = match.group(2)
+        json_str = match[-1][1]
 
     # Strip whitespace and newlines from the start and end
     json_str = json_str.strip()
